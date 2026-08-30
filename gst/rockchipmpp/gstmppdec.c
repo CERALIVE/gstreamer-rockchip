b1c6844eddfe2c416ab7bb0904c7878b2e8427db
andrescera <Andres.cera@hotmail.com>
2026-08-30 15:03:47 -0500

    fix(dec): pass the correct GstMeta pointer to gst_buffer_remove_meta

--- gst/rockchipmpp/gstmppdec.c
+++ gst/rockchipmpp/gstmppdec.c
@@ -949,7 +949,7 @@ gst_mpp_dec_rga_convert (GstVideoDecoder * decoder, MppFrame mframe,
 
   if (crop && ret) {
     // We've already cropped the frame with RGA, remove the crop meta
-    gst_buffer_remove_meta(buffer, crop);
+    gst_buffer_remove_meta (buffer, &crop->meta);
   }
 
   return ret;
